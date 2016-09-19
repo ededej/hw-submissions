36cbcf708f3434b4e09450765e2aa8c13c1868f1
Richard <zhangruiqian5@gmail.com>
2016-09-19 17:20:50 -0400

    updated signup js

--- signUp.js
+++ signUp.js
@@ -1,82 +1,107 @@
-<<<<<<< HEAD
+// window.onbeforeunload = function (e) {
+//     e = e || window.event;
+//
+//     if (e) {
+//         e.returnValue = 'Sure?';
+//     }
+//
+//     return 'Sure?';
+// };
+window.onbeforeunload = function(evt) {
+    var message = 'Did you remember to download your form?';
+    if (typeof evt == 'undefined') {
+        evt = window.event;
+    }
+    if (evt) {
+        evt.returnValue = message;
+    }
+
+    return message;
+}
 function SubmitPopUp()
 {
-    showMessageDialog("Thank you for sign up!\n\nClick 'OK' to sign in from main page!","normal");
-    // if (confirm("Thank you for sign up!\n\nClick 'OK' to sign in from main page!") == true) {
-    //     //var win = window.open("index.html", '_blank');
-    //     window.location.href = "index.html";
-    //     win.focus();
-    //     } else {
-    //
-    //     }
+    // alert("Thank you for signing up!");
+    // window.location.href = "index.html";
+    BootstrapDialog.show({
+            type: BootstrapDialog.TYPE_PRIMARY,
+            title:'Message',
+            message: "Thank you for signing up!",
+            buttons: [{
+                label: 'OK',
+                cssClass: 'btn-primary',
+                action: function(dialogItself){
+                    dialogItself.close();
+                    window.location.href = "index.html";
+                }
+            }]
+        });
+
 }
 function GoBack() {
 
     showMessageDialog("Do you want to go back to last page?","warning");
 }
 
-function showMessageDialog(message,type)
-{
-    if(type == "error")
-    {
-
-        BootstrapDialog.show({
-            title: 'Error',
-            type: BootstrapDialog.TYPE_DANGER,
-            message: message,
-            buttons: [{
-                label: 'Close',
-                action: function(dialogItself){
-                    dialogItself.close();
-                }
-            }]
-        });
-    }else if(type == "warning")
-    {
-        BootstrapDialog.show({
-            title: 'Warning',
-            message: message,
-            type: BootstrapDialog.TYPE_WARNING,
-            buttons: [
-                {
-                    label: 'OK',
-                    cssClass: 'btn-warning',
-                    action: function (dialogItself) {
-                        history.back();
-                    }
-                },
-                {
-                    label: 'Close',
-                    cssClass: 'btn-danger',
-                    action: function(dialogItself){
-                        dialogItself.close();
-                    }
-                }]
-        });
-    }else if(type == "normal")
-    {
-        BootstrapDialog.show({
-            title: 'Message',
-            type: BootstrapDialog.TYPE_PRIMARY,
-            message: message,
-            buttons: [
-                {
-                    label: 'OK',
-                    cssClass: 'btn-primary',
-                    action: function (dialogItself) {
-                        window.location.href = "index.html";
-                    }
-                },
-                {
-                    label: 'Close',
-                    cssClass: 'btn-warning',
-                    action: function(dialogItself){
-                        dialogItself.close();
-                }
-            }]
-
-        });
-    }
-}
-=======
->>>>>>> ededej/hw2
+// function showMessageDialog(message,type)
+// {
+//     if(type == "error")
+//     {
+//
+//         BootstrapDialog.show({
+//             title: 'Error',
+//             type: BootstrapDialog.TYPE_DANGER,
+//             message: message,
+//             buttons: [{
+//                 label: 'Close',
+//                 action: function(dialogItself){
+//                     dialogItself.close();
+//                 }
+//             }]
+//         });
+//     }else if(type == "warning")
+//     {
+//         BootstrapDialog.show({
+//             title: 'Warning',
+//             message: message,
+//             type: BootstrapDialog.TYPE_WARNING,
+//             buttons: [
+//                 {
+//                     label: 'OK',
+//                     cssClass: 'btn-warning',
+//                     action: function (dialogItself) {
+//                         history.back();
+//                     }
+//                 },
+//                 {
+//                     label: 'Close',
+//                     cssClass: 'btn-danger',
+//                     action: function(dialogItself){
+//                         dialogItself.close();
+//                     }
+//                 }]
+//         });
+//     }else if(type == "normal")
+//     {
+//         BootstrapDialog.show({
+//             title: 'Message',
+//             type: BootstrapDialog.TYPE_PRIMARY,
+//             message: message,
+//             buttons: [
+//                 {
+//                     label: 'OK',
+//                     cssClass: 'btn-primary',
+//                     action: function (dialogItself) {
+//                         window.location.href = "index.html";
+//                     }
+//                 },
+//                 {
+//                     label: 'Close',
+//                     cssClass: 'btn-warning',
+//                     action: function(dialogItself){
+//                         dialogItself.close();
+//                 }
+//             }]
+//
+//         });
+//     }
+// }
