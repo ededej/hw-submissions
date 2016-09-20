626cb4ce73432147215e97c06b0ed9a9f21931f6
ededej <ededej@masonlive.gmu.edu>
2016-09-20 10:10:17 -0400

    release 2

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 
-// This function it redirect to the page
+// This function it redirect to the page and save the information about login in local storage to use on ASP
+// on singIn page
 function SubmitPopUp()
 {
     window.location.href = "signIn.html";
     var userID = document.getElementById('inputEmail').value;
     var userPass=document.getElementById('inputPassword').value;
     localStorage.setItem("userName", userID);
-
-    // saveUserLogin(userID,userPass);
     // alert("Thank you for signing up, "+ userID);
 
-}
-
-function saveUserLogin(user, pass) {
-    var account = {
-        User: user,
-        Pass: pass
-    };
-    //converts to JSON string the Object
-    account = JSON.stringify(account);
-    //creates a base-64 encoded ASCII string
-    // account = btoa(account);
-    //save the encoded accout to web storage
-    localStorage.setItem('_account', account);
 }
