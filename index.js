7c642ae9b7fbda9bdeb248b0b9ede82bd46ad481
ededej <ededej@masonlive.gmu.edu>
2016-09-20 09:52:25 -0400

    release 1

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 
-$(document).ready(function() {
-    $("#login").click(function loginFunc() {
-        var name = document.getElementById('inputEmail').value;
-        // alert("Welcome, " + name);
-        // open("signIn.html");
-        // window.location.assign("signIn.html");
-        window.location.href='signIn.html';
+// This function it redirect to the page
+function SubmitPopUp()
+{
+    window.location.href = "signIn.html";
+    var userID = document.getElementById('inputEmail').value;
+    var userPass=document.getElementById('inputPassword').value;
+    localStorage.setItem("userName", userID);
 
-    });
+    // saveUserLogin(userID,userPass);
+    // alert("Thank you for signing up, "+ userID);
 
-});
+}
 
-function welcomUser{
-
+function saveUserLogin(user, pass) {
+    var account = {
+        User: user,
+        Pass: pass
+    };
+    //converts to JSON string the Object
+    account = JSON.stringify(account);
+    //creates a base-64 encoded ASCII string
+    // account = btoa(account);
+    //save the encoded accout to web storage
+    localStorage.setItem('_account', account);
 }
--- signIn.js
+++ signIn.js
@@ -1,43 +1,83 @@
- $(document).ready(function() {
+// This is our single page website which will responsive to the user interaction with it.
+// I have use javascript and jquery to create the view and append the text into view.
+// I will use the history manipulation function to go back and forward to different views.
+// Also I have create an array of object to be display on the view each time the user likes
+// to view them.
 
-    var counterFav=0;
+$(document).ready(function() {
+
+     var counterFav=0;
      var counterSugg=0;
+     var stateObj = { };
 
-     // showfavorites();
-     // showSuggestions();
+    // I will get the userName that I stored on login information from index.js
+     var name = localStorage.getItem("userName");
+    // if a name is provide by login if not display the defult message just User
+     if (name){
+         $("#userMsg").append("Welcome, "+name); // here display the name of the user
+     }else{
+         $("#userMsg").append("Welcome, "+ "User");
+
+     }
+
+     // here I have the event lister for a click on the so I display only what
+     // the user choose to display
      $("#favBtn").click(function (){
-         if(counterFav==0) {
-             showfavorites();
+         if(counterFav==0) { // creating the view if no view have been created
+             // history.pushState(null, null, "#favRes");
+             stateObj = { view: "favRes" };
+             showfavorites();// calling the showfavorites to crete the view
+             // just displaying with hide and show the favorite
              $("#suggRes").hide();
              $("#favRes").show();
              counterFav++;
-         }else{
+         }else{// just displaying with hide and show the favorite
              $("#suggRes").hide();
              $("#favRes").show();
 
          }
-         // $("#suggTable").hide();
      });
      $("#suggBtn").click(function (){
          if(counterSugg==0) {
+             // history.pushState(null, null, "#suggRes");
              $("#favRes").hide();
-             showSuggestions();
+             showSuggestions();// calling the showSuggestions to crete the view
+             // just displaying with hide and show the suggestion
              $("#suggRes").show();
              counterSugg++;
-         }else{
+         }else{// just displaying with hide and show the suggestions
              $("#favRes").hide();
              $("#suggRes").show();
 
          }
      });
-     $("#contBtn").click(function (){
+     // $("#contBtn").click(function (){
+     //
+     // });
 
-     });
+    // Creating the history bottom
+    $(window).on("popstate", function() {
+         // alert("on popstate");
+    });
 
- });
+    $("#favBtn").click(function(e) {
+        e.preventDefault();
+        // alert("on cliclfunctiot 1");
+        window.history.pushState("", document.title, "#favRes");
+    });
+    $("#suggBtn").click(function(e) {
+        e.preventDefault();
+        // alert("on clikc function 2");
+        window.history.pushState("", document.title, "#suggRes");
+    });
 
+
+
+});
+
+ // showfavorites it creates a table of view of the favorite resutrant stored on array restarunts
 function showfavorites() {
-    $('#favRes').append("<h4> Your favorite places to eat</h4>");
+    $('#favRes').prepend("<h4> Your favorite places to eat</h4>");
 
     //The head of the table
     $('#favTable').append("<tr> <th>Restaurant</th> <th>Type of Food</th> <th>Location</th> </tr>");
@@ -61,7 +101,6 @@ function showfavorites() {
     ];
 // The loop to display the information into the table to append
     for (var restaurant of restaurants) {
-         // $('#favTable').append(' Name: ' + restaurant.name + ' Address: ');
          $('#favTable').append(
              "<tr> <td>"+restaurant.name +"</td>"
              + "<td>"+restaurant.typeOfFood +"</td> "
@@ -71,10 +110,10 @@ function showfavorites() {
     }
 }
 
-
+ // showSuggestions it creates a table of view of the suggestion made by resutrant stored on array restarunts
  function showSuggestions() {
-
-     $('#suggRes').append("<h4> Our suggestion for you today</h4>");
+    // The message for the table title
+     $('#suggRes').prepend("<div id='title' ></div><h4> Our suggestion for you today</h4></div>");
 
      //The head of the table
      $('#suggTable').append("<tr> <th>Restaurant</th> <th>Type of Food</th> <th>Location</th> </tr>");
@@ -98,7 +137,6 @@ function showfavorites() {
      ];
 // The loop to display the information into the table to append
      for (var restaurant of restaurants) {
-         // $('#favTable').append(' Name: ' + restaurant.name + ' Address: ');
          $('#suggTable').append(
              "<tr> <td>"+restaurant.name +"</td>"
              + "<td>"+restaurant.typeOfFood +"</td> "
