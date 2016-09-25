0d560eb3dc078bbb8b9957fe9d81bb1a3e15e9d5
ededej <ededej@masonlive.gmu.edu>
2016-09-25 10:25:24 -0400

    Add changes for index.html and singUp.html
    1. Made the singUp to not register a user with same email address on
    database.
    2. Displays the message to the user that the account already exists.
    3. Add server.js  and other node.js modules for our server.
    4. Added on server.js facet API to get data from their web services.

--- public/index.js
+++ public/index.js
@@ -1,12 +1,35 @@
 
 // This function it redirect to the page and save the information about login in local storage to use on ASP
 // on singIn page
-function SubmitPopUp()
-{
-    window.location.href = "signIn.html";
-    var userID = document.getElementById('inputEmail').value;
-    var userPass=document.getElementById('inputPassword').value;
-    localStorage.setItem("userName", userID);
-    // alert("Thank you for signing up, "+ userID);
+// function SubmitPopUp()
+// {
+//     window.location.href = "signIn.html";
+//     var userID = document.getElementById('inputEmail').value;
+//     var userPass=document.getElementById('inputPassword').value;
+//     localStorage.setItem("userName", userID);
+//     // alert("Thank you for signing up, "+ userID);
+//
+// }
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+// var Factual = require('factual-api');
+// var factual = new Factual('8t9ZdXYwXills2o1Ftf2XepEmrfFHHWaJmftl0DS', 'BpDpxPzl4M2IPfb7C0Fkqj9p44SQCvsbbBdf1akq');
+//
+// factual.get('http://api.v3.factual.com/t/restaurants-us/schema', function (error, res) {
+//     console.log(res.view);
+// });
 
-}
+// $(document).ready(function() {
+//
+//     $.getJSON("http://api.v3.factual.com/t/restaurants-us/schema", function (data) {
+//         var items = [];
+//         $.each(data, function (key, val) {
+//             // items.push("<li id='" + key + "'>" + val + "</li>");
+//             $("#results").appendTo("<li id='" + key + "'>" + val + "</li>");
+//
+//         });
+//
+//     });
+//
+// }); // done ready
--- public/signUp.js
+++ public/signUp.js
@@ -1,17 +1,146 @@
-// singUp.js it will simple since the singUp.html is just a form to singup on our page
+"use strict";
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyASLA-lv8N5CtE1orATmWy7uINmH0ZSdMA",
+    authDomain: "pranzousersdb.firebaseapp.com",
+    databaseURL: "https://pranzousersdb.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "694440498360"
+};
 
-//This function it will pup an alert to the user to not close the tab/window without saving the informatio
-// window.onbeforeunload = function(event) {
-//     event.returnValue = alert("");
-// };
-// $(window).on('beforeunload', function(){
-//     return "Good bye";
-// });
+firebase.initializeApp(config);
 
-//The SubmitPopUp just rederect the to the signIn.html and thanks the user for sing in.
-// function SubmitPopUp()
-// {
-//     window.location.href = "signIn.html";
-//     alert("Thank you for signing up!");
+var usersRef= firebase.database().ref('users');
+
+var verifyEmail = document.getElementById("emailId2");
+if(verifyEmail) {
+    verifyEmail.addEventListener("keyup", function (event) {
+        if (!getEmailId) {
+            verifyEmail.setCustomValidity("I expect an e-mail, darling!");
+        } else {
+            verifyEmail.setCustomValidity("");
+        }
+    });
+}
+
+$('#singUp').click(function(){
+    var uid=document.getElementById('emailId1').value;
+    var gender=getGenderValue();
+    var foodPrefer= getFoodPref();
+    var elemState = document.getElementById("state");
+    var state = elemState.options[elemState.selectedIndex].text;
+//        var emailId= getEmailId();
+    if (uid == null || uid == "") {
+        alert("Email must be filled out");
+        return false;
+    }else {
+        uid=uid.replace(/\./g, '%20');
+        usersRef.child(uid).set(
+            {
+                FirstName: document.getElementById('firstName').value,
+                LastName: document.getElementById('lastName').value,
+                Gender: gender,
+                Diet: foodPrefer,
+                Email:uid,
+                Password: '',
+                Address: {
+                    Street: document.getElementById('streetId').value,
+                    City: document.getElementById('cityId').value,
+                    State: state,
+                    ZipCode: document.getElementById('zipCode').value
+                }
+
+            }
+        )
+    }
+});
+
+
+
+//    function verifyEmail(input) {
+//        input = input.srcElement;
+//        if (input.value != document.getElementById('emailId1').value) {
+//            // the provided value doesn’t match the primary email address
+//            input.setCustomValidity('The two email addresses must match.');
+//        } else {
+//            // input is valid -- reset the error message
+//            input.setCustomValidity('');
+//        }
+//    }
+function getGenderValue(){
+    var gender=$('input[name=gender]:checked').val();
+    if(gender){
+        return gender;
+    }else{
+        return "None"
+    }
+}
+
+function getFoodPref() {
+    var prefer=$('input[name=foodOption]:checked').val();
+    if(prefer){
+        return prefer;
+    }else{
+        return "None"
+    }
+}
+function getEmailId() {
+    var email1= document.getElementById('emailId1').value;
+    var email2= document.getElementById('emailId2').value;
+    if(email1===email2){
+        return true;
+    }else{
+        return false;
+
+    }
+
+
+}
+
+//    function SubmitPopUp()
+//    {
+//        window.location.href = "signIn.html";
+//        alert("Thank you for signing up!");
 //
-// }
+//    }
+//    var todosRef = firebase.database().ref('todos');
+//
+//
+//    var nextIndex = 0;
+//
+//    todosRef.on('child_added', function(data) {
+//        createItem(data.val().text, data.key);
+//    });
+//    todosRef.on('child_changed', function(data) {
+//        $('div[data-index="' + data.key + '"] input').val(data.val().text);
+//    });
+//    todosRef.on('child_removed', function(data) {
+//        $('div[data-index="' + data.key + '"]').remove();
+//    });
+
+
+//    $('#new').click(function()
+//    {
+//        todosRef.push({ 'text': ""});
+//    });
+//
+//    function createItem(value, key)
+//    {
+//        $('#todoItems').append(
+//                '<div class="todoItem" data-index="' + key+ '"><input type="text" onchange="itemChanged(this)" value="'
+//                + value + '"><button onclick="deleteItem(this.parentElement)">&#x2716;</button></div>'
+//        );
+//    }
+
+//    function deleteItem(inputDiv)
+//    {
+//        todosRef.child(inputDiv.dataset.index).remove();
+//    }
+//
+//    function itemChanged(inputElem)
+//    {
+//        var index = inputElem.parentElement.dataset.index;
+//        todosRef.child(inputElem.parentElement.dataset.index).set({ 'text': inputElem.value });
+//    }
+
+
